9229d66736a4f6a5920de8b06ba60d000c5b65ee
Matthew Hughes <matt.h17@yahoo.com>
2024-05-31 19:39:03 +0000

    Added profile app, django restframework

---
+++ drf_api/profiles/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../<ADD Your image name from cloudinary(don’t forget the hash)>', blank=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+
+    
+
+
